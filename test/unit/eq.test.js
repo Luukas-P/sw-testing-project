9aae271cea78104ba260bfba2c2176ac270ad228
Riikka Alatalkkari <riikka.alatalkkari@tuni.fi>
2025-11-30 23:00:24 +0200

    Creating multiple testcases

--- test/unit/eq.test.js
+++ test/unit/eq.test.js
@@ -3,7 +3,50 @@ import eq from '../../src/eq.js';
 
 describe('eq.js Tests', () => {
     
-    it('should return [1, 4, 9] when squaring [1, 2, 3]', () => {
-        const result = map([1, 2, 3], (x) => x * x);
-        expect(result).to.deep.equal([1, 4, 9]);
+    it('should return true when object1 & object2 is the same string', () => {
+        const result = eq("Strawberry","Strawberry");
+        expect(result).to.equal(true);
     });
+
+    it('should return true when object1 & object2 is the same number', () => {
+        const result = eq(8,8);
+        expect(result).to.equal(true);
+    });
+
+    it('should return true when object1 & object2 is the same float', () => {
+        const result = eq(1.02,1.02);
+        expect(result).to.equal(true);
+    });
+
+    it('should return true when object1 & object2 is the same char', () => {
+        const result = eq(a,a);
+        expect(result).to.equal(true);
+    });
+
+    it('should return true when object1 & object2 is Nan', () => {
+        const result = eq(NaN,NaN);
+        expect(result).to.equal(true);
+    });
+
+    it('should return false when object1 & object2 different strings', () => {
+        const result = eq("Strawberry","Blueberry");
+        expect(result).to.equal(false);
+    });
+
+    it('should return false when object1 is string & object2 is number', () => {
+        const result = eq("Strawberry",2);
+        expect(result).to.equal(false);
+    });
+
+    it('should return false when object1 & object2 is different numbers', () => {
+        const result = eq(8,2);
+        expect(result).to.equal(false);
+    });
+
+    it('should return false when object1 is string & object2 is number', () => {
+        const result = eq("Strawberry",2);
+        expect(result).to.equal(false);
+    });
+
+
+});
