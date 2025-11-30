d8b9690abaa39bdd25f4965f4056a12c7a4c5c5b
Riikka Alatalkkari <riikka.alatalkkari@tuni.fi>
2025-11-30 19:41:31 +0200

    Test commit

--- test/unit/eq.test.js
+++ test/unit/eq.test.js
@@ -1,2 +1,9 @@
 import { expect } from 'chai';
 import eq from '../../src/eq.js';
+
+describe('eq.js Tests', () => {
+    
+    it('should return [1, 4, 9] when squaring [1, 2, 3]', () => {
+        const result = map([1, 2, 3], (x) => x * x);
+        expect(result).to.deep.equal([1, 4, 9]);
+    });
