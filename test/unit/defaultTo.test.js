31a604dce27a182c97c3befcec8a417e675a6412
Riikka Alatalkkari <riikka.alatalkkari@tuni.fi>
2025-12-01 20:29:46 +0200

    Test case functions added

--- test/unit/defaultTo.test.js
+++ test/unit/defaultTo.test.js
@@ -1,2 +1,36 @@
 import { expect } from 'chai';
 import defaultTo from '../../src/defaultTo.js';
+
+describe('defaultTo.js Tests', () => {
+    
+    it('Function should return the number (obj1) when its defined', () => {
+        const result = defaultTo(1,5);
+        expect(result).to.equal(1);
+    });
+
+    it('Function should return the string (obj1) when its defined', () => {
+        const result = defaultTo("Good quality","Neutral");
+        expect(result).to.equal("Good quality");
+    });
+
+    it('Function should return the negative number (obj1) when its defined', () => {
+        const result = defaultTo(-1,5);
+        expect(result).to.equal(-1);
+    });
+
+    it('Function should return the default value (obj2) when obj1 is undefined', () => {
+        const result = defaultTo(undefined,5);
+        expect(result).to.equal(5);
+    });
+
+    it('Function should return the default value (obj2) when obj1 is NaN', () => {
+        const result = defaultTo(NaN,5);
+        expect(result).to.equal(5);
+    });
+
+    it('Function should return the default value (obj2) when obj1 is null', () => {
+        const result = defaultTo(null,5);
+        expect(result).to.equal(5);
+    });
+
+});
