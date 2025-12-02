35287e69b8bc88dd5a3e3ac1349e0702739ad9cd
Riikka Alatalkkari <riikka.alatalkkari@tuni.fi>
2025-12-02 12:25:47 +0200

    Creating multiple test cases

--- test/unit/isEmpty.test.js
+++ test/unit/isEmpty.test.js
@@ -1,2 +1,51 @@
 import { expect } from 'chai';
 import isEmpty from '../../src/isEmpty.js';
+
+describe('isEmpty.js Tests', () => {
+    
+    it('should return true when object is null', () => {
+        const result = isEmpty(null);
+        expect(result).to.equal(true);
+    });
+
+    it('should return true when object is empty set', () => {
+        const result = isEmpty([]);
+        expect(result).to.equal(true);
+    });
+
+    it('should return true when object is empty map', () => {
+        const result = isEmpty({});
+        expect(result).to.equal(true);
+    });
+
+    it('should return true when object is true', () => {
+        const result = isEmpty(true);
+        expect(result).to.equal(true);
+    });
+
+    it('should return true when object is undefined', () => {
+        const result = isEmpty(undefined);
+        expect(result).to.equal(true);
+    });
+
+    it('should return true when object is NaN', () => {
+        const result = isEmpty(NaN);
+        expect(result).to.equal(true);
+    });
+
+    it('should return false when object is set with elements', () => {
+        const result = isEmpty([1,2,3]);
+        expect(result).to.equal(false);
+    });
+
+    it('should return false when object is map with elements', () => {
+        const result = isEmpty({'a':1});
+        expect(result).to.equal(false);
+    });
+
+    it('should return false when object is string', () => {
+        const result = isEmpty(('abc'));
+        expect(result).to.equal(false);
+    });
+
+});
